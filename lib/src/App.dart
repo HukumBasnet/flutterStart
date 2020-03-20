438e2bfa2edffba87261a98a3dd1250e9cf26546
HukumBasnet <es.bijay.upreti@gmail.com>
2020-03-20 17:16:06 +0545

    simple animation

--- lib/src/App.dart
+++ lib/src/App.dart
@@ -1,34 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'screens/home.dart';
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // final wordPair = WordPair.random();
+  Widget build(context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          // child: Text('Hello World'),
-          child: RandomWords(),
-        ),
-      ),
+      title: 'Animation',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Home(),
     );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
   }
 }
