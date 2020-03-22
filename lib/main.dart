00efed82ae93ff001619f3c73e9e7082da62029d
reenahukum <big.b.it.traininig@gmail.com>
2020-03-22 20:03:24 +0545

    add first style

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,19 @@ void main() => runApp(MaterialApp(
         appBar: AppBar(
           title: Text('my first app'),
           centerTitle: true,
+          backgroundColor: Colors.pink[600],
         ),
         body: Center(
-          child: Text('hello, latoo'),
+          child: Text('hello, latoo',
+              style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.0,
+                  color: Colors.grey[600],
+                  fontFamily: 'IndieFlower')),
         ),
         floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.red[600],
           child: Text('click'),
         ),
       ),
