8684e3a3db96f4974696fba2813700b2bc054ab5
reenahukum <big.b.it.traininig@gmail.com>
2020-03-22 19:55:44 +0545

    changing some stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Text('hey ninjas!!')));
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('my first app'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text('hello, latoo'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text('click'),
+        ),
+      ),
+    ));
